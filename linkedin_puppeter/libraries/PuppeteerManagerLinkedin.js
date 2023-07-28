1a30fe42d11b813da8bf3d9bd5a22c1fd0c314db
Himanshu Rehani <himanshurehani@nethues.com>
2023-07-28 18:52:31 +0530

    completed the populating fields with params sent, will continue working on scrapping jobs data.

--- linkedin_puppeter/libraries/PuppeteerManagerLinkedin.js
+++ linkedin_puppeter/libraries/PuppeteerManagerLinkedin.js
@@ -59,20 +59,63 @@ class PuppeteerManagerLinkedin {
             case "searchJobs":
                 try { 
                     /*Start populating fields first*/
-                    
+                    //click in title field and add title of job
+                    await page.waitForSelector('input[aria-label="Search by title, skill, or company"]');
+                    await page.click('input[aria-label="Search by title, skill, or company"]');
+                    await this.sleep(2000);
+
+                    await page.keyboard.type(this.jobTitle, { delay: 100 });
+                    await this.sleep(2000);
+
+                    //click in location field and add location of job
+                    await page.waitForSelector('input[aria-label="City, state, or zip code"]');
+                    await page.click('input[aria-label="City, state, or zip code"]');
+                    await this.sleep(2000);
+
+                    const input = await page.$('input[aria-label="City, state, or zip code"]');
+                    await input.click({ clickCount: 3 });
+                    await this.sleep(2000);
+
+                    await page.keyboard.type(this.location, { delay: 100 });
+                    await this.sleep(2000);
+
+                    //click on search button
+                    await page.waitForXPath('//button[text()="Search"]');
+                    const searchButton = await page.$x('//button[text()="Search"]');
+                    await page.evaluate(el => el.click(), searchButton[0]);
+                    await this.sleep(2000);
+
                     // date posted
-                    /*await page.waitForXPath('//button[contains(text(), "Date posted")]');
-                    const getDatePostButton = await page.$x('//button[contains(text(), "Date posted")]');
-                    await page.evaluate(el => el.click(), getDatePostButton[0]);*/
+                    await page.waitForXPath('//button[text()="Date posted"]');
+                    const getDatePostButton = await page.$x('//button[text()="Date posted"]');
+                    await page.evaluate(el => el.click(), getDatePostButton[0]);
 
-                    await page.waitForSelector('div[aria-label="Date posted filter. Clicking this button displays all Date posted filter options."]');
-                    await page.click('div[aria-label="Date posted filter. Clicking this button displays all Date posted filter options."]');
-                    this.sleep(2000);
+                    await page.waitForXPath('//span[text()= "'+this.date_posted+'"]');
+                    const selectedDatePosted = await page.$x('//span[text()= "'+this.date_posted+'"]');
+                    await page.evaluate(el => el.click(), selectedDatePosted[0]);
 
-                    //type
-                    await page.waitForSelector('div[aria-label="On-site/remote filter. Clicking this button displays all On-site/remote filter options."]');
-                    await page.click('div[aria-label="On-site/remote filter. Clicking this button displays all On-site/remote filter options."]');
-                    this.sleep(2000);
+                    // select date filter result
+                    await page.waitForXPath('/html/body/div[5]/div[3]/div[4]/section/div/section/div/div/div/ul/li[3]/div/div/div/div[1]/div/form/fieldset/div[2]/button[2]');
+                    const selectDatePostedResult = await page.$x('/html/body/div[5]/div[3]/div[4]/section/div/section/div/div/div/ul/li[3]/div/div/div/div[1]/div/form/fieldset/div[2]/button[2]');
+                    await page.evaluate(el => el.click(), selectDatePostedResult[0]);
+                    await this.sleep(4000);
+                    
+                    //type 
+                    await page.waitForXPath('//div[@data-basic-filter-parameter-name="workplaceType"]//button[@aria-label="On-site/remote filter. Clicking this button displays all On-site/remote filter options."]');
+                    const getWorkPlaceTypeButton = await page.$x('//div[@data-basic-filter-parameter-name="workplaceType"]//button[@aria-label="On-site/remote filter. Clicking this button displays all On-site/remote filter options."]');
+                    await page.evaluate(el => el.click(), getWorkPlaceTypeButton[0]);
+                    await this.sleep(2000);
+
+                    await page.waitForXPath('//span[text()= "'+this.type+'"]');
+                    const selectedWorkingType = await page.$x('//span[text()= "'+this.type+'"]');
+                    await page.evaluate(el => el.click(), selectedWorkingType[0]);
+                    await this.sleep(2000);
+
+                    // select type filter result
+                    await page.waitForXPath('/html/body/div[5]/div[3]/div[4]/section/div/section/div/div/div/ul/li[7]/div/div/div/div[1]/div/form/fieldset/div[2]/button[2]');
+                    const selectTypeResult = await page.$x('/html/body/div[5]/div[3]/div[4]/section/div/section/div/div/div/ul/li[7]/div/div/div/div[1]/div/form/fieldset/div[2]/button[2]');
+                    await page.evaluate(el => el.click(), selectTypeResult[0]);
+                    await this.sleep(6000);
                     
                     this.return = "Data found..!!";
                     return true
